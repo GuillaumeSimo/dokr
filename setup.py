61899a85b01e356f090b714ea1cc57c7e6c5e257
Guillaume SIMO <guillaume.simo@decathlon.com>
2020-07-22 18:44:53 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='dokr',  
+     version='0.1',
+     scripts=['dokr'] ,
+     author="Guillaume Simo",
+     author_email="guillaume.simo@hotmal.fr",
+     description="A AWS utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/guillaumesimo/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
